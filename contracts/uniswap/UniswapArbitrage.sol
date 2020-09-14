420a4066b5c04c399254b6d9d5c125c55368c27f
huy@gohuygo.com <huy@gohuygo.com>
2020-09-13 21:39:26 -0700

    uniswap

---
+++ contracts/uniswap/UniswapArbitrage.sol
@@ -0,0 +1,42 @@
+pragma solidity =0.6.6;
+
+import "./libraries/SafeMath.sol";
+import './libraries/UniswapV2Library.sol';
+
+import "./interfaces/IUniswapV2Router02.sol";
+import "./interfaces/IERC20.sol";
+
+contract UniswapArbitrage {
+    address internal constant UNISWAP_ROUTER_ADDRESS = 0x7a250d5630B4cF539739dF2C5dAcb4c659F2488D;
+    address internal constant WETH_ADDRESS = 0xa0f764E120459bca39dB7E57a0cE975a489aB4fa;
+    IUniswapV2Router02 internal uniswapRouter;
+    
+    constructor() public {
+        uniswapRouter = IUniswapV2Router02(UNISWAP_ROUTER_ADDRESS);
+    }
+    
+    function trade(uint256 wethAmount) public {
+
+        address[] memory path = new address[](2);
+        path[0] = address(0xa0f764E120459bca39dB7E57a0cE975a489aB4fa); //WETH
+        path[1] = address(0x9657ff14c2D6d502113FDAD8166d1c14c085C2eC); //MYX
+
+        
+        // Approve the pool from the ERC20 pairs for the ProxyArbitrage smart contract
+        approveContract(path[0], wethAmount);
+        approveContract(path[1], wethAmount);
+
+        uint deadline = block.timestamp + 30; 
+        // TODO: Min Token calc
+        uniswapRouter.swapExactTokensForTokensSupportingFeeOnTransferTokens(
+            100_000, 0, path, address(this), deadline);
+        
+    }
+
+    function approveContract(address tokenAddress, uint256 amount) public {
+        IERC20 token = IERC20(tokenAddress);
+        if (token.allowance(address(this), UNISWAP_ROUTER_ADDRESS) < amount) {
+            token.approve(UNISWAP_ROUTER_ADDRESS, 99999999999 ether);
+        }
+    }
+}
